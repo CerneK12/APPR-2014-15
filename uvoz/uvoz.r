4d9262b279a0c44da04c5bfecea92c9db4520311
Katarina Černe <katka.cerne@gmail.com>
2014-12-02 15:23:00 +0100

    uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -10,8 +10,8 @@ uvoziObsojeni <- function() {
                       fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o Obsojenih fizičnah osebah po občinah v Sloveniji...\n")
+# Zapišimo podatke v razpredelnico obsojenih fizičnih osebah v Sloveniji.
+cat("Uvažam podatke o Obsojenih fizičnih osebah po občinah v Sloveniji...\n")
 obsojeni <- uvoziObsojeni()
 
 
@@ -25,9 +25,23 @@ uvoziDiplomanti <- function() {
                     fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
+# Zapišimo podatke v razpredelnico diplomanti.
 cat("Uvažam podatke o diplomantih...\n")
 diplomanti <- uvoziDiplomanti()
+
+
+#Tabela BREZPOSELNIH
+
+# Funkcija, ki uvozi podatke iz datoteke diplomanti.csv
+uvoziBrezposelni <- function() {
+  return(read.table("podatki/brezposelni.csv", sep = ";", as.is = TRUE,
+                    col.names = c("obcina", "2006", "2007", "2008", "2009", "2010", "2011", "2012","2013"),
+                    fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico breposelni.
+cat("Uvažam podatke o brezposelnih...\n")
+brezposelni <- uvoziBrezposelni()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
