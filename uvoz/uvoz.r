7af85f0b4eda67301cce644dc5922bdb9876508d
Katarina Černe <katka.cerne@gmail.com>
2014-12-02 15:01:19 +0100

    združitev repozitorijev

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,16 +1,33 @@
 # 2. faza: Uvoz podatkov
 
+
+#Tabela OBSOJENI
+
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
+uvoziObsojeni <- function() {
+  return(read.table("podatki/obsojeni.csv", sep = ";", as.is = TRUE,
+                      col.names = c("obcina", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"),
                       fileEncoding = "Windows-1250"))
 }
 
 # Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+cat("Uvažam podatke o Obsojenih fizičnah osebah po občinah v Sloveniji...\n")
+obsojeni <- uvoziObsojeni()
+
+
+
+#Tabela DIPLOMANTI
+
+# Funkcija, ki uvozi podatke iz datoteke diplomanti.csv
+uvoziDiplomanti <- function() {
+  return(read.table("podatki/diplomanti.csv", sep = ";", as.is = TRUE,
+                    col.names = c("obcina", "2006", "2007", "2008", "2009", "2010", "2011", "2012","2013"),
+                    fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam podatke o diplomantih...\n")
+diplomanti <- uvoziDiplomanti()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
