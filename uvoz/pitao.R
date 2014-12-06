60183139ac54e0e2a82f148451f85f23b2eee663
Katarina Černe <katka.cerne@gmail.com>
2014-12-06 20:20:46 +0100

    dodani naslovi

--- uvoz/pitao.R
+++ uvoz/pitao.R
@@ -1,4 +1,4 @@
 pdf("slike/leto_2007_obsojeni.pdf")
-pie(obsojeni[,"X2007"], labels = obsojeni[,1])
+pie(obsojeni[,"X2007"], labels = obsojeni[,1], main="OBSOJENE FIZICNE OSEBE PO REGIJAH")
 text(-1.5,1,"2007", cex =2)
 dev.off()
