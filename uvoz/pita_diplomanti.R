eb1d6b1f9d0cffc494eb54ea0b4badb276beacb3
Katarina Černe <katka.cerne@gmail.com>
2015-03-29 17:23:27 +0200

    popraavljena velikost pisave

--- uvoz/pita_diplomanti.R
+++ uvoz/pita_diplomanti.R
@@ -3,7 +3,7 @@
 pdf("slike/leto_2007_diplomanti.pdf")
 pie(diplomanti[,"X2007"],
     labels = diplomanti[,1],
-    radius = 0.6,
+    cex=0.8,
     main="DELEZ DIPLOMANTOV PO REGIJAH, 2007",
     col=rainbow(12))
 dev.off()
--- uvoz/pita_obsojeni.R
+++ uvoz/pita_obsojeni.R
@@ -3,7 +3,7 @@
 pdf("slike/leto_2007_obsojeni.pdf")
 pie(obsojeni[,"X2007"],
     labels = obsojeni[,1],
-    radius = 0.6,
+    cex=0.8,
     main="DELEZ OBSOJENIH FIZICNIH OSEB PO REGIJAH, 2007",
     col=rainbow(12))
 dev.off()
