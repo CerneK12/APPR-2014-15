7c68a53166b1965716b32bd2d7034e27c76a7fd8
Katarina Černe <katka.cerne@gmail.com>
2015-03-29 16:24:30 +0200

    zmanjšana pita

--- uvoz/pita_diplomanti.R
+++ uvoz/pita_diplomanti.R
@@ -1,11 +1,9 @@
 #pita diplomanti
 
-
-
-
 pdf("slike/leto_2007_diplomanti.pdf")
 pie(diplomanti[,"X2007"],
     labels = diplomanti[,1],
+    radius = 0.6,
     main="DELEZ DIPLOMANTOV PO REGIJAH, 2007",
     col=rainbow(12))
 dev.off()
--- uvoz/pita_obsojeni.R
+++ uvoz/pita_obsojeni.R
@@ -1,10 +1,9 @@
 #pita obsojeni
 
-
-
 pdf("slike/leto_2007_obsojeni.pdf")
 pie(obsojeni[,"X2007"],
     labels = obsojeni[,1],
+    radius = 0.6,
     main="DELEZ OBSOJENIH FIZICNIH OSEB PO REGIJAH, 2007",
     col=rainbow(12))
 dev.off()
